682e55c559b31be4cb6b23434c9778901fff3f62
Santi Porollan <santiporollan@gmail.com>
2019-09-27 20:05:56 -0300

    ahora devuelve el tablero

--- sudoku.py
+++ sudoku.py
@@ -2,23 +2,23 @@ class Sudoku:
     def __init__(self, board):
         self.original_board = board
         self.board = board
-        self.over = False
-        
-    
+
     def game_is_over(self):
         self.over = True
         for x in range(len(self.board[0])):
             for y in range(len(self.board[0])):
-                 if self.board[x][y] == "x":
-                     self.over = False
-                     break
-        return self.over
+                if self.board[x][y] == "x":
+                    return False
+        return True
 
     def place_number(self, number, X, Y):
         number = str(number)
-        if self.original_board[X][Y] != 'x':
-            return False
 
+        # comprobar si el lugar esta disponible
+        if self.original_board[X][Y] != 'x':
+            return self.board
+
+        # comprobar si se repite en el bloque
         block_X = X // 3
         block_Y = Y // 3
         if block_X == 1:
@@ -29,18 +29,20 @@ class Sudoku:
             block_X += 4
         if block_Y == 2:
             block_Y += 4
-        
         for x in range(block_X, block_X + 3):
             for y in range(block_Y, block_Y + 3):
                 if self.board[x][y] == number:
-                    return False
+                    return self.board
 
-        f = list(self.board[X])
-        f[Y] = number
+        # comprobar si se repite en la fila/columna
+        for x in range(len(self.board[0])):
+            if self.board[X][x] == number or self.board[x][Y] == number:
+                return self.board
+
+        # colocar el numero
+        f = list(self.board[Y])
+        f[X] = number
         s = ''.join(f)
-        self.board[X] = s
+        self.board[Y] = s
 
-        return True
-            
-
-
+        return self.board
