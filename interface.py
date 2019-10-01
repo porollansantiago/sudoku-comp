66ab8e775e8bab77be258ee79c5012a59da13d62
Santi Porollan <santiporollan@gmail.com>
2019-10-01 11:38:27 -0300

    modificado para buscar el estado del juego

--- interface.py
+++ interface.py
@@ -2,27 +2,20 @@ from sudoku import Sudoku
 
 
 class Interface:
-    def __init__(self):
-        self.game = Sudoku(["53xx7xxxx",
-                            "6xx195xxx",
-                            "x98xxxx6x",
-                            "8xxx6xxx3",
-                            "4xx8x3xx1",
-                            "7xxx2xxx6",
-                            "x6xxxx28x",
-                            "xxx419xx5",
-                            "xxxx8xx79"])
+    def __init__(self, board):
+        self.game = Sudoku(board)
 
     def play(self, number, X, Y):
 
         try:
             number, X, Y = int(number), int(X), int(Y)
         except ValueError:
-            return "Solo se pueden ingresar numeros"
+            return "Solo se pueden ingresar numeros", self.game.game_is_over()
 
         if number not in range(1, 10):
-            return "El numero va desde 1 a 9"
+            return "El numero va desde 1 a 9", self.game.game_is_over()
         if X not in range(0, 9) or Y not in range(0, 9):
-            return "Las coordenadas van del 0 al 8"
+            return "Las coordenadas van del 0 al 8", self.game.game_is_over()
 
-        return self.game.place_number(str(number), str(X), str(Y))
+        return (self.game.place_number(str(number), str(X), str(Y)),
+                self.game.game_is_over())
