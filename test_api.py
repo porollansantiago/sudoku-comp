bd7b47052d117222c1ccee140a3f83d6e08db166
Santi Porollan <santiporollan@gmail.com>
2019-10-02 11:12:43 -0300

    agregado test_api

---
+++ test_api.py
@@ -0,0 +1,47 @@
+import unittest
+from api import Api_acceso
+
+
+class Test_api_acceso(unittest.TestCase):
+    def setUp(self):
+        self.api = Api_acceso()
+        self.board = self.api.get_new_board()
+
+    # def test_get_valid_board(self):
+    #     board = self.api.get_new_board()
+
+    def test_board_not_null(self):
+        self.assertTrue(self.board)
+
+    def test_board_has_numbers(self):
+        for x in range(9):
+            for y in range(9):
+                if self.board[x][y] != 'x':
+                    flag = True
+                    break
+        self.assertTrue(flag)
+
+    def test_valid_rows(self):
+        for y in range(9):
+            row = []
+            for x in range(9):
+                if self.board[y][x] != 'x':
+                    row.append(self.board[y][x])
+            s = set(row)
+            self.assertEqual(len(row), len(s))
+
+    def test_valid_cols(self):
+        for y in range(9):
+            col = []
+            for x in range(9):
+                if self.board[x][y] != 'x':
+                    col.append(self.board[x][y])
+            s = set(col)
+            self.assertEqual(len(col), len(s))
+
+    def test_valid_blocks(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
