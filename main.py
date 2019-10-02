6000ac90e024b9faffbf7522900689b0738ae890
Santi Porollan <santiporollan@gmail.com>
2019-10-02 10:35:45 -0300

    pequeño cambio por api

--- main.py
+++ main.py
@@ -2,18 +2,9 @@ from interface import Interface
 
 
 def main():
-    board = ["53xx7xxxx",
-             "6xx195xxx",
-             "x98xxxx6x",
-             "8xxx6xxx3",
-             "4xx8x3xx1",
-             "7xxx2xxx6",
-             "x6xxxx28x",
-             "xxx419xx5",
-             "xxxx8xx79"]
 
-    interface_sudoku = Interface(board)
-    show_board(board)
+    interface_sudoku = Interface()
+    show_board(interface_sudoku.get_board())
     over = False
 
     while not over:
