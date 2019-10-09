abe8ab531bfd660c7317db7182d36552feb26b83
Santi Porollan <santiporollan@gmail.com>
2019-10-09 10:02:07 -0300

    agregados tests tablas 4 x 4

--- test_sudoku.py
+++ test_sudoku.py
@@ -13,7 +13,12 @@ class Test_sudoku(unittest.TestCase):
                             "x6xxxx28x",
                             "xxx419xx5",
                             "xxxx8xx79"])
+        self.orig_board_44 = (["x2xx",
+                               "3xx1",
+                               "x32x",
+                               "4xxx"])
         self.game = Sudoku(self.orig_board)
+        self.game44 = Sudoku(self.orig_board_44)
 
     def test_place_invalid_number1(self):
         number, Y, X = 1, 0, 0
@@ -317,6 +322,94 @@ class Test_sudoku(unittest.TestCase):
                                      "x6xxxx28x",
                                      "xxx419xx5",
                                      "x4xx8xx79"])
+
+    def test_place_invalid_number_block41(self):
+        number, Y, X = 2, 3, 3
+        board = self.game44.place_number(number, X, Y)
+        new_board = []
+        for val in board:
+            new_board.append("".join(val))
+        self.assertEqual(new_board, ["x2xx",
+                                     "3xx1",
+                                     "x32x",
+                                     "4xxx"])
+
+    def test_place_invalid_number_row41(self):
+        number, Y, X = 1, 1, 1
+        board = self.game44.place_number(number, X, Y)
+        new_board = []
+        for val in board:
+            new_board.append("".join(val))
+        self.assertEqual(new_board, ["x2xx",
+                                     "3xx1",
+                                     "x32x",
+                                     "4xxx"])
+
+    def test_place_valid_number41(self):
+        number, Y, X = 1, 0, 0
+        board = self.game44.place_number(number, X, Y)
+        new_board = []
+        for val in board:
+            new_board.append("".join(val))
+        self.assertEqual(new_board, ["12xx",
+                                     "3xx1",
+                                     "x32x",
+                                     "4xxx"])
+
+    def test_place_valid_number42(self):
+        number, Y, X = 3, 0, 2
+        board = self.game44.place_number(number, X, Y)
+        new_board = []
+        for val in board:
+            new_board.append("".join(val))
+        self.assertEqual(new_board, ["x23x",
+                                     "3xx1",
+                                     "x32x",
+                                     "4xxx"])
+
+    def test_place_valid_number4(self):
+        number, Y, X = 3, 3, 3
+        board = self.game44.place_number(number, X, Y)
+        new_board = []
+        for val in board:
+            new_board.append("".join(val))
+        self.assertEqual(new_board, ["x2xx",
+                                     "3xx1",
+                                     "x32x",
+                                     "4xx3"])
+
+    def test_game_not_over4(self):
+        over = self.game44.game_is_over()
+        self.assertFalse(over)
+
+    def test_game_not_over_after_play4(self):
+        number, Y, X = 3, 3, 3
+        self.game44.place_number(number, X, Y)
+        over = self.game.game_is_over()
+        self.assertFalse(over)
+
+    def test_game_over_after_play4(self):
+        full_board = Sudoku(["2134",
+                             "3421",
+                             "1342",
+                             "421x"])
+        number, Y, X = 3, 3, 3
+        full_board.place_number(number, X, Y)
+        over = full_board.game_is_over()
+        self.assertTrue(over)
+
+    def test_replace_placed_number41(self):
+        number, Y, X = 3, 0, 2
+        board = self.game44.place_number(number, X, Y)
+        number, Y, X = 4, 0, 2
+        board = self.game44.place_number(number, X, Y)
+        new_board = []
+        for val in board:
+            new_board.append("".join(val))
+        self.assertEqual(new_board, ["x24x",
+                                     "3xx1",
+                                     "x32x",
+                                     "4xxx"])
 
 
 if __name__ == "__main__":
