8854e7b16dbf0619bc1f8484915b83f21d90a2f0
Santi Porollan <santiporollan@gmail.com>
2019-10-09 11:51:57 -0300

    ahora usa la longitud del board

--- interface.py
+++ interface.py
@@ -17,7 +17,7 @@ class Interface:
             return "El numero va desde 1 a 9", self.game.game_is_over()
         if X not in range(0, len(self.board)) or Y not in range(0, len(self.board)):
             return "Las coordenadas van del 1 al 9", self.game.game_is_over()
-        return (self.game.place_number(str(number), str(X), str(Y)),
+        return (self.game.place_number(str(number), X, Y),
                 self.game.game_is_over())
 
     def get_board(self):
