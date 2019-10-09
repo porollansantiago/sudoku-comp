8fa10703fcdbcb136a1d7b39561b3c9a9c3a372f
Santi Porollan <santiporollan@gmail.com>
2019-10-09 10:15:43 -0300

    ahora soporta tableros 4x4

--- interface.py
+++ interface.py
@@ -7,16 +7,15 @@ class Interface:
         self.game = Sudoku(self.board)
 
     def play(self, number, X, Y):
-
         try:
             number, X, Y = int(number), int(X), int(Y)
         except ValueError:
             return "Solo se pueden ingresar numeros", self.game.game_is_over()
-        Y = 9 - Y
+        Y = len(self.board) - Y
         X = X - 1
-        if number not in range(1, 10):
+        if number not in range(1, len(self.board) + 1):
             return "El numero va desde 1 a 9", self.game.game_is_over()
-        if X not in range(0, 9) or Y not in range(0, 9):
+        if X not in range(0, len(self.board)) or Y not in range(0, len(self.board)):
             return "Las coordenadas van del 1 al 9", self.game.game_is_over()
         return (self.game.place_number(str(number), str(X), str(Y)),
                 self.game.game_is_over())
