3e3e08f36c7319101fd8e8261a9a02d1e1399c6a
Santi Porollan <santiporollan@gmail.com>
2019-10-09 10:01:50 -0300

    ahora suporta tablas 4 x 4

--- sudoku.py
+++ sudoku.py
@@ -4,7 +4,7 @@ import copy
 class Sudoku:
     def __init__(self, board):
         if type(board[0]) is str:
-            for val in range(9):
+            for val in range(len(board[0])):
                 board[val] = list(board[val])
         self.original_board = copy.deepcopy(board)
         self.board = board
@@ -21,6 +21,19 @@ class Sudoku:
                     return
         self.over = True
 
+    def get_block_number(self, X, Y, blocks):
+        block_X = X // blocks
+        block_Y = Y // blocks
+        if block_X == 1:
+            block_X += blocks - 1
+        elif block_X == 2:
+            block_X += ((blocks - 1) * 2)
+        if block_Y == 1:
+            block_Y += blocks - 1
+        elif block_Y == 2:
+            block_Y += ((blocks - 1) * 2)
+        return block_Y, block_X
+
     def place_number(self, number, X, Y):
         number = str(number)
         X = int(X)
@@ -31,23 +44,15 @@ class Sudoku:
             return self.board
 
         # comprobar si se repite en el bloque
-        block_X = X // 3
-        block_Y = Y // 3
-        if block_X == 1:
-            block_X += 2
-        if block_Y == 1:
-            block_Y += 2
-        if block_X == 2:
-            block_X += 4
-        if block_Y == 2:
-            block_Y += 4
-        for y in range(block_Y, block_Y + 3):
-            for x in range(block_X, block_X + 3):
+        blocks = 3 if len(self.board[0]) == 9 else 2
+        block_Y, block_X = self.get_block_number(X, Y, blocks)
+        for y in range(block_Y, block_Y + blocks):
+            for x in range(block_X, block_X + blocks):
                 if self.board[y][x] == number:
                     return self.board
 
         # comprobar si se repite en la fila/columna
-        for x in range(8):
+        for x in range(len(self.board)):
             if self.board[Y][x] == number or self.board[x][X] == number:
                 return self.board
 
@@ -55,5 +60,4 @@ class Sudoku:
         self.board[Y][X] = number
 
         self.check_full_board()
-
         return self.board
