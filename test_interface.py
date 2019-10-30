4ee11ab3d7d42d7d1432bbd99b7aadbe1a315035
Santi Porollan <santiporollan@gmail.com>
2019-10-29 23:22:10 -0300

    listos tests parameterized

--- test_interface.py
+++ test_interface.py
@@ -1,5 +1,6 @@
 import unittest
 from interface import Interface
+from parameterized import parameterized
 
 
 class Test_interface(unittest.TestCase):
@@ -14,48 +15,29 @@ class Test_interface(unittest.TestCase):
                                     "xxx419xx5",
                                     "xxxx8xx79"])
 
-    def test_number_not_in_range1(self):
-        number, Y, X = 0, 1, 1
+    @parameterized.expand([
+        (0, 1, 1),
+        (10, 1, 1),
+        (-1, 1, 1),
+        (10000, 4, 4),
+        (-1234, 1, 3),
+    ])
+    def test_number_not_in_range(self, number, Y, X):
         play, is_over = self.interface.play(number, X, Y)
         self.assertEqual(play, "El numero va desde 1 a 9")
 
-    def test_number_not_in_range2(self):
-        number, Y, X = 10, 1, 1
-        play, is_over = self.interface.play(number, X, Y)
-        self.assertEqual(play, "El numero va desde 1 a 9")
-
-    def test_number_not_in_range3(self):
-        number, Y, X = -1, 1, 1
-        play, is_over = self.interface.play(number, X, Y)
-        self.assertEqual(play, "El numero va desde 1 a 9")
-
-    def test_XY_not_in_range1(self):
-        number, Y,  X = 1, 10, 0
+    @parameterized.expand([
+        (1, 10, 0),
+        (1, 0, 10),
+        (1, 55, 55),
+        (1, -43, 55),
+    ])
+    def test_XY_not_in_range(self, number, Y, X):
         play, is_over = self.interface.play(number, X, Y)
         self.assertEqual(play, "Las coordenadas van del 1 al 9")
 
-    def test_XY_not_in_range2(self):
-        number, Y,  X = 1, 0, 10
-        play, is_over = self.interface.play(number, X, Y)
-        self.assertEqual(play, "Las coordenadas van del 1 al 9")
-
-    def test_XY_not_in_range3(self):
-        number, Y,  X = 1, 55, 55
-        play, is_over = self.interface.play(number, X, Y)
-        self.assertEqual(play, "Las coordenadas van del 1 al 9")
-
-    def test_XY_not_in_range4(self):
-        number, Y,  X = 1, -43, 55
-        play, is_over = self.interface.play(number, X, Y)
-        self.assertEqual(play, "Las coordenadas van del 1 al 9")
-
-    def test_number_placed1(self):
-        number, Y, X = 2, 9, 9
-        play, is_over = self.interface.play(number, X, Y)
-        new_board = []
-        for val in play:
-            new_board.append("".join(val))
-        self.assertEqual(new_board, [
+    @parameterized.expand([
+        (2, 9, 9, [
             "Y",
             "9    5 3 _ | _ 7 _ | _ _ 2 ",
             "8    6 _ _ | 1 9 5 | _ _ _ ",
@@ -68,15 +50,8 @@ class Test_interface(unittest.TestCase):
             "3    _ 6 _ | _ _ _ | 2 8 _ ",
             "2    _ _ _ | 4 1 9 | _ _ 5 ",
             "1    _ _ _ | _ 8 _ | _ 7 9 ",
-            "\n     1 2 3   4 5 6   7 8 9  X"])
-
-    def test_number_placed2(self):
-        number, Y, X = 2, 1, 1
-        play, is_over = self.interface.play(number, X, Y)
-        new_board = []
-        for val in play:
-            new_board.append("".join(val))
-        self.assertEqual(new_board, [
+            "\n     1 2 3   4 5 6   7 8 9  X"]),
+        (2, 1, 1, [
             "Y",
             "9    5 3 _ | _ 7 _ | _ _ _ ",
             "8    6 _ _ | 1 9 5 | _ _ _ ",
@@ -89,15 +64,8 @@ class Test_interface(unittest.TestCase):
             "3    _ 6 _ | _ _ _ | 2 8 _ ",
             "2    _ _ _ | 4 1 9 | _ _ 5 ",
             "1    2 _ _ | _ 8 _ | _ 7 9 ",
-            "\n     1 2 3   4 5 6   7 8 9  X"])
-
-    def test_place_valid_number3(self):
-        number, Y, X = 1, 7, 1
-        play, is_over = self.interface.play(number, X, Y)
-        new_board = []
-        for val in play:
-            new_board.append("".join(val))
-        self.assertEqual(new_board, [
+            "\n     1 2 3   4 5 6   7 8 9  X"]),
+        (1, 7, 1, [
             "Y",
             "9    5 3 _ | _ 7 _ | _ _ _ ",
             "8    6 _ _ | 1 9 5 | _ _ _ ",
@@ -110,20 +78,22 @@ class Test_interface(unittest.TestCase):
             "3    _ 6 _ | _ _ _ | 2 8 _ ",
             "2    _ _ _ | 4 1 9 | _ _ 5 ",
             "1    _ _ _ | _ 8 _ | _ 7 9 ",
-            "\n     1 2 3   4 5 6   7 8 9  X"])
+            "\n     1 2 3   4 5 6   7 8 9  X"]),
 
-    def test_letters1(self):
-        number, Y, X = 1, 2, 'X'
+    ])
+    def test_number_placed(self, number, Y, X, expected):
         play, is_over = self.interface.play(number, X, Y)
-        self.assertEqual(play, "Solo se pueden ingresar numeros")
+        new_board = []
+        for val in play:
+            new_board.append("".join(val))
+        self.assertEqual(new_board, expected)
 
-    def test_letters2(self):
-        number, Y, X = 1, 'X', 0
-        play, is_over = self.interface.play(number, X, Y)
-        self.assertEqual(play, "Solo se pueden ingresar numeros")
-
-    def test_letters3(self):
-        number, Y, X = 'X', 2, 0
+    @parameterized.expand([
+        (1, 2, 'X'),
+        (1, 'X', 0),
+        ('X', 2, 0),
+    ])
+    def test_letters(self, number, Y, X):
         play, is_over = self.interface.play(number, X, Y)
         self.assertEqual(play, "Solo se pueden ingresar numeros")
 
